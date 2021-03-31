52b493d87ae1b76ae828333d24745220a5cc67c9
Vidura Fernando <heyvidura@gmail.com>
2021-03-31 15:46:42 +0530

    Initialized Project 😃

---
+++ springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/SpringbootteamworkApplication.java
@@ -0,0 +1,13 @@
+package com.heymeowcat.springbootteamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootteamworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootteamworkApplication.class, args);
+	}
+
+}
---
+++ springbootteamwork/src/test/java/com/heymeowcat/springbootteamwork/SpringbootteamworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heymeowcat.springbootteamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootteamworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
